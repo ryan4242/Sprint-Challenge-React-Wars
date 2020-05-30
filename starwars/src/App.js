dcd3ebd09da98d3d297ee55acb743e10a1b1df9e
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-05-29 19:09:32 -0700

    api calls are working and painting data to screen

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 import './App.css';
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.py4e.com/api/people/')
+    .then(response => {
+      console.log(response);
+      setCharacters(response.data.results);
+    })
+  }, [])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  console.log('state', characters);
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(char => {
+        return <Character key={char.url} char={char}/>
+      })}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+
+const Character = ({char}) => {
+  const [planet, setPlanet] = useState('');
+  const [species, setSpecies] = useState('')
+  const planetCall = char.homeworld;
+  const speciesCall = char.species[0];
+
+  useEffect(() => {
+    
+    axios.get(planetCall)
+    .then(response => {
+      setPlanet(response.data.name)
+    });
+
+    axios.get(speciesCall)
+    .then(response => {
+      setSpecies(response.data.name)
+    })
+  }, [planetCall, speciesCall])
+
+  return(
+    <div>
+      <h3>{char.name}</h3>
+      <p>Species: {species}</p>
+      <p>Born on: {planet}</p>
+    </div>
+  )
+}
+
+export default Character;
