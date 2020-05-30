b1342fcc1f1dbef1a1b4767f1a3b8044a68b8436
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-05-29 19:46:26 -0700

    added a search form to filter characters

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import Search from './components/Search'
 import Character from './components/Character';
 import styled from 'styled-components';
 import './App.css';
@@ -14,32 +15,25 @@ const Container = styled.div`
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
-    axios.get('https://swapi.py4e.com/api/people/')
+    axios.get(`https://swapi.py4e.com/api/people/${search}`)
     .then(response => {
-      console.log(response);
+      console.log(search);
       setCharacters(response.data.results);
     })
-  }, [])
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-  console.log('state', characters);
-
+  }, [search]);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Search set={setSearch} />
       <Container>
         {characters.map(char => {
           return <Character key={char.url} char={char}/>
         })}
-      </Container>
-      
+      </Container>     
     </div>
   );
 }
